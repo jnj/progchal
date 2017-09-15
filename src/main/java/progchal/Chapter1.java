17a1619d64ad11e73051ab16bb76d780901ae879
Josh Joyce <j_joyce2001@yahoo.com>
2017-09-14 22:11:09 -0500

    starting 1.6 problems

--- src/main/java/progchal/Chapter1.java
+++ src/main/java/progchal/Chapter1.java
@@ -9,7 +9,7 @@ public class Chapter1 {
     /**
      * 1.6.1
      */
-    public static int maxCycleLength(int i, int j) {
+    static int maxCycleLength(int i, int j) {
         int max = 0;
 
         for (int k = i; k <= j; k++) {
@@ -33,5 +33,59 @@ public class Chapter1 {
         } while (i != 1);
 
         return count;
+    }
+
+    /**
+     * 1.6.2
+     */
+    static class Minesweeper {
+        private static int MINE = -1;
+        private final int w;
+        private final int h;
+        private final int[] field;
+
+        Minesweeper(int rows, int cols) {
+            this.w = cols;
+            this.h = rows;
+            this.field = new int[rows * cols];
+        }
+
+        void putMineAt(int row, int col) {
+            field[row * w + col] = MINE;
+
+            for (int i = row - 1; i <= row + 1; i++) {
+                for (int j = col - 1; j <= col + 1; j++) {
+                    if (i != row || j != col) {
+                        if (i >= 0 && i < h && j >= 0 && j < w) {
+                            int index = i * w + j;
+                            if (field[index] != MINE) {
+                                field[index]++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        char getCountAt(int row, int col) {
+            int count = field[row * w + col];
+            return count == MINE ? '*' : Character.forDigit(count, 10);
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder b = new StringBuilder();
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    b.append(getCountAt(i, j));
+                }
+                if (i < h - 1) {
+                    b.append("\n");
+                }
+            }
+
+            return b.toString();
+        }
     }
 }
--- src/test/java/progchal/Chapter1Test.java
+++ src/test/java/progchal/Chapter1Test.java
@@ -14,4 +14,32 @@ public class Chapter1Test {
         assertEquals(89, maxCycleLength(201, 210));
         assertEquals(174, maxCycleLength(900, 1000));
     }
+
+    @Test
+    public void minesweeper1() {
+        Chapter1.Minesweeper ms = new Chapter1.Minesweeper(4, 4);
+        ms.putMineAt(0, 0);
+        ms.putMineAt(2, 1);
+        assertEquals(
+                "*100\n" +
+                "2210\n" +
+                "1*10\n" +
+                "1110",
+                ms.toString()
+        );
+    }
+
+    @Test
+    public void minesweeper2() {
+        Chapter1.Minesweeper ms = new Chapter1.Minesweeper(3, 5);
+        ms.putMineAt(0, 0);
+        ms.putMineAt(0, 1);
+        ms.putMineAt(2, 1);
+        assertEquals(
+                "**100\n" +
+                "33200\n" +
+                "1*100",
+                ms.toString()
+        );
+    }
 }
